75f54cfbd2a0e32d13a32f8e96393a519dd05b6f
Munkh-Altai Chuluunbaatar <munkaltai@gmail.com>
2021-11-10 13:33:29 +0800

    bug fixed

--- Moqup.go
+++ Moqup.go
@@ -3,7 +3,6 @@ package moqup
 import (
 	"html/template"
 	"github.com/lambda-platform/moqup/handler"
-	"github.com/lambda-platform/moqup/utils"
 	"github.com/labstack/echo/v4"
 	lambdaUtils "github.com/lambda-platform/lambda/utils"
 	templateUtils "github.com/lambda-platform/moqup/utils"
@@ -13,12 +12,11 @@ import (
 func Set(e *echo.Echo){
 
 	templates := lambdaUtils.GetTemplates(e)
-	AbsolutePath := utils.AbsolutePath()
-	TemplatePath := templateUtils.AbsolutePath()
-	templates["moqup.html"] = template.Must(template.ParseFiles(AbsolutePath+"templates/moqup.html"))
 
-	templates["adminmodule.html"] = template.Must(template.ParseFiles(
-		TemplatePath+"views/paper.html",
+	TemplatePath := templateUtils.AbsolutePath()
+
+	templates["moqup.html"] = template.Must(template.ParseFiles(
+		TemplatePath+"templates/moqup.html",
 	))
 	e.GET("/pages/moqup/:id", handler.Moqup)
 
